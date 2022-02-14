8d93fa67e0fffecbdb4bef8c73cf1862f2b07d3e
Lee Jordan <duracell80@users.noreply.github.com>
2022-02-13 21:37:08 -0600

    Update s2s.sh

--- s2s.sh
+++ s2s.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+echo -e "\n \n- Data checked every 5 to 15 minutes"
+echo -e "- Run in Chron or with & to background"
+echo -e "- Press [CTRL+C] to stop \n \n"
+ 
 while true
 do
- echo -e "\n \n- Data checked every 5 to 15 minutes"
- echo -e "- Run in Chron or with & to background"
- echo -e "- Press [CTRL+C] to stop \n \n"
  python3 storm2shutdown.py
- sleep 600
 done
