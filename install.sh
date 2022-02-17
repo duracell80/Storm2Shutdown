3711505b8eb2cb5cdee318dad4b40908c7ab4fd5
Lee Jordan <duracell80@users.noreply.github.com>
2022-02-16 20:47:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y powertop
 sudo apt-get install -y python3-pip
 
-sudo pip3 install feedparser
+sudo pip3 install --upgrade feedparser
 sudo pip3 install --upgrade numpy
-sudo pip3 install pandas
+sudo pip3 install --upgrade pandas
