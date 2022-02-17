f1a11c9a852264b5bc166646f0d68417aa8cedc2
duracell80 <lee.jordan@gmail.com>
2022-02-16 21:05:16 -0600

    Delete check.sh

--- check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#URL="https://alerts.weather.gov/cap/fl.php?x=0"
-
-#ENTS=$(cat us.xml | grep -o '<entry>[^<]*' | grep -o "[^>]*$")
-#SUMS=$(cat us.xml | grep -o '<summary>[^<]*' | grep -o "[^>]*$")
-
-#echo $ENTS
-
